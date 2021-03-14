98eee42c5401cae17ada1cfad4157ad047350c31
head-node <76886903+head-node@users.noreply.github.com>
2021-03-14 20:41:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from 'react';
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  constructor(props){
+ super(props);
+  this.state={
+    flag:false
+  }
+  } 
+  change=()=>{
+    this.setState({
+   flag:true
+    })
+  }
+  render() {
+    return (
+      <div id="main">
+        <button onClick={this.change}>Click</button> 
+       { this.state.flag?<p id="para"> Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+</p>:<></>}
+      </div>
+    )
+  } 
 }
 
-
 export default App;
-
